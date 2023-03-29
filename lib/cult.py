8a2cd85d0fa20af400cc2bf6d3fe16e24d3d2f49
Kimberly Benton <kimberly.d.benton@gmail.com>
2023-03-29 15:34:48 -0400

    issues

--- lib/cult.py
+++ lib/cult.py
@@ -1,48 +1,34 @@
 from .bloodoath import BloodOath
 
 class Cult:
-    
+
     all = []
 
-    def __init__(self, name, city, founding_year, slogan):
+    def __init__(self, name, city, year, slogan):
         self.name = name
         self.city = city
-        self.year = founding_year
+        self.year = year
         self.slogan = slogan
         Cult.all.append(self)
 
     @property
     def oaths(self):
-        return [ o for o in BloodOath.all if o.cult == self]
+        return[o for o in BloodOath.all if o.cult == self]
     
     @property
     def followers(self):
-        return [ o.follower for o in self.oaths]
-    
+        return[o for o in self.oaths]
+
     def recruit_follower(self, follower):
-        BloodOath("2023-03-31", self, follower)
-
-        # how to check : len(c1.followers)---2
-        # adam = Follower("adam", 43, "cant stop wont stop")--
-        # len(c1.followers)---3
-        # c1.followers[-1].name---'adam'
-
-    @property
+        return BloodOath("2012-12-12", self, follower )
+    
     def cult_population(self):
         return len(self.followers)
-    
+
     @classmethod
     def find_by_name(cls, name_string):
         for c in cls.all:
             if c.name == name_string:
                 return c
             else:
-                return "Cult Not Found"
-    
-    @classmethod    
-    def find_by_location(cls, location_string):
-        return [c for c in cls.all if c.city == location_string]
-    
-    @classmethod
-    def find_by_founding_year(cls,found_year):
-        return [ c for c in cls.all if c.year == found_year]
+                return 'Cult Not Found'
--- lib/follower.py
+++ lib/follower.py
@@ -3,8 +3,8 @@ from .bloodoath import BloodOath
 class Follower:
 
     all = []
-    
-    def __init__(self, name, age, life_motto):
+
+    def __init__(self, name, age, life_motto ):
         self.name = name
         self.age = age
         self.life_motto = life_motto
@@ -17,20 +17,7 @@ class Follower:
             if oath.follower == self:
                 oath_list.append(oath)
         return oath_list
-
+    
     @property
     def cults(self):
-        return [o.cult for o in self.oaths]
-    
-
-    def join_cult(self, cult_instance):
-        BloodOath("1984", cult_instance, self)
-
-        # len(f3.cults) --- 2
-        #f3.join_cults(c2) ----
-        # len(f3.cults) --- 3
-
-    @classmethod
-    def of_a_certain_age(cls, age_int):
-        return [ f for f in cls.all if f.age >= age_int]
-    
+        return [ o for o in self.oaths]
